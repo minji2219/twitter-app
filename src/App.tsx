4807143387a3e1f46ef60fa40bdb036004bec4f3
minji2219 <200146@jnu.ac.kr>
2024-02-06 23:30:38 +0900

    page생성 및 router 연결
    필요한 페이지 모두 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-
+import Router from "components/Router";
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<h1>Home Page</h1>} />
-      <Route path="/posts" element={<h1>Post List Page</h1>} />
-      <Route path="/posts/:id" element={<h1>Post Detail Page</h1>} />
-      <Route path="/posts/new" element={<h1>Post Edit Page</h1>} />
-      <Route path="/profile" element={<h1>Profile Page</h1>} />
-      <Route path="/profile/edit" element={<h1>profile edit Page</h1>} />
-      <Route path="/notifications" element={<h1>notifications edit Page</h1>} />
-      <Route path="/search" element={<h1>search Page</h1>} />
-      <Route path="/user/login" element={<h1>login Page</h1>} />
-      <Route path="/user/signup" element={<h1>signup Page</h1>} />
-      <Route path="*" element={<Navigate replace to="/" />} />
-    </Routes>
-  );
+  return <Router />;
 }
 
 export default App;
---
+++ src/pages/posts/edit.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostEdit = () => {
+  return <div></div>;
+};
+
+export default PostEdit;
